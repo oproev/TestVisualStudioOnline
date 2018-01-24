6de5de2cbfd8ff9b2989ae05281cc3f3b5e3eeed
Odne Pedersen Røv <odne@enso.no>
2018-01-24 10:35:56 -0800

    Added Selenium tests

---
+++ SolutionsTests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace SolutionsTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestA()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.demoqa.com";
+
+            Assert.IsTrue(driver.Title == "Demoqa | Just another WordPress site");
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+
+        [Test]
+        public void TestB()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.demoqa.com";
+
+            Assert.IsTrue(driver.Title == "Demoqa | Just another WordPress site");
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+    }
+}
