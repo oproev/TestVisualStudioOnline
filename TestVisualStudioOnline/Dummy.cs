7101d97b460424ee5457fbf06f8590494b3e5b03
Odne Pedersen Røv <odne@enso.no>
2018-01-24 09:41:41 -0800

    Change class name

---
+++ TestVisualStudioOnline/Dummy.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace TestVisualStudioOnline
+{
+    public class Dummy
+    {
+        public Dummy()
+        {
+            Debug.WriteLine("Test code");
+        }
+    }
+}
