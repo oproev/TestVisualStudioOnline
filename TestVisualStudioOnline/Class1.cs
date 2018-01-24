cedb9b2301a49d2d643c6012cceb39dc78792201
Odne Pedersen Røv <odne@enso.no>
2018-01-24 09:30:56 -0800

    new fix

--- TestVisualStudioOnline/Class1.cs
+++ TestVisualStudioOnline/Class1.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace TestVisualStudioOnline
 {
     public class Class1
     {
+        public Class1()
+        {
+            Debug.WriteLine("Test code");
+        }
     }
 }
